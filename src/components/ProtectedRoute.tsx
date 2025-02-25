733970a3efb64c2977c11d1079c5c5823bc44bbf
Ajay Yadav <141677772+Byte0017@users.noreply.github.com>
2025-02-25 19:44:09 +0530

    Delete src directory

--- src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from '../context/AuthContext';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const { user } = useAuth();
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
