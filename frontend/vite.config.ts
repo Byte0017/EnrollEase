5dc8ee476eee75cab23dbc233b769bbd48e06b60
Ajay Yadav <141677772+Byte0017@users.noreply.github.com>
2025-02-25 20:19:41 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/EnrollEase/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
